40e7fe5b0942b9e14fe51c29cd7e9a2661635c01
syeddhassannaali <syeddhassannaali@gmail.com>
2026-01-12 10:26:40 +0500

    first commit

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,13 +1,14 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 
-import { CustomButton01 } from "../partials/custom-buttons";
+import { Button } from "../ui/button";
+
+import { cn } from "@/lib/utils";
 
 import Logo from "../../../public/favicons/logo-transparent.png";
 import MenuIcon from "@/assets/icons/menu-icon.svg";
-import { cn } from "@/lib/utils";
 
 function Header() {
   const pathname = usePathname();
@@ -74,7 +75,14 @@ function Header() {
           </div>
 
           <div className="flex gap-4 items-center">
-            <CustomButton01>Get Started</CustomButton01>
+            <Button className="py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff] relative">
+              <div className="absolute inset-0">
+                <div className="rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]" />
+                <div className="rounded-lg border border-white/40 absolute inset-0 [mask-image:linear-gradient(to_top,black,transparent)]" />
+                <div className="absolute inset-0 shadow-[0_0_10px_rgba(140,69,255,.7)_inset] rounded-lg" />
+              </div>
+              <span>Get Started</span>
+            </Button>
 
             <Image src={MenuIcon} alt="menu" className="md:hidden" />
           </div>
